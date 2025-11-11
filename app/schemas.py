97febfc87dc62828b93a0fe8ca0cbac46aa4c98a
OzMaralez <oz.5.1.14.10.18@gmail.com>
2025-11-11 19:50:17 +0300

    fix: schema BookRead

--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,8 @@
+from uuid import UUID
 from datetime import date
 from typing import Optional
 
 from pydantic import BaseModel, Field, constr, conint
-
-
-#Набор ограничений
-Title = constr(max_length=200)
-Author = constr(max_length=100)
-Year = conint(ge=1000, le=date.today().year)
-
 
 
 class BookBase(BaseModel):
@@ -28,7 +22,7 @@ class BookUpdate(BookBase):
 
 
 class BookRead(BookBase):
-    id: int
+    id: UUID
 
     class Config:
         orm_mode = True
