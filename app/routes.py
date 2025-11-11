faa3d964506f3b592bcd4fd61045f3f01c1db324
OzMaralez <oz.5.1.14.10.18@gmail.com>
2025-11-11 19:22:27 +0300

    fix: routes.py Fixed dependency injection from get_db() to get_db

--- app/routes.py
+++ app/routes.py
@@ -14,7 +14,7 @@ router = APIRouter(prefix='/books', tags=["Books"])
 @router.post('/', response_model=BookRead, status_code=201)
 def create_book(
         payload: BookCreate,
-        db: Session = Depends(get_db())
+        db: Session = Depends(get_db)
 ):
     return db_repo.create_book(db, payload)
 
@@ -23,7 +23,7 @@ def create_book(
 def list_books(
         skip: int = Query(0, ge=0),
         limit: int = Query(10, gt=0, le=100),
-        db: Session = Depends(get_db())
+        db: Session = Depends(get_db)
 ):
     return db_repo.list_books(db, skip, limit)
 
@@ -31,7 +31,7 @@ def list_books(
 @router.get('/{book_id}', response_model=BookRead)
 def get_book(
         book_id: UUID,
-        db: Session = Depends(get_db())
+        db: Session = Depends(get_db)
 ):
     book = db_repo.get_book(db, book_id)
 
@@ -45,7 +45,7 @@ def get_book(
 def update_book(
         payload: BookUpdate,
         book_id: UUID,
-        db: Session = Depends(get_db())
+        db: Session = Depends(get_db)
 ):
     updated_book = db_repo.update_book(db, book_id, payload)
 
@@ -58,7 +58,7 @@ def update_book(
 @router.delete('/{book_id}', status_code=204)
 def delete_book(
         book_id: UUID,
-        db: Session = Depends(get_db())
+        db: Session = Depends(get_db)
 ):
     ok = db_repo.delete_book(db, book_id)
 
